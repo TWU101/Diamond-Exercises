4fca0186af0f47038cdfc28b51c7240361e968b9
mm <mauricio.machicado@gmail.com>
2016-03-28 10:44:50 -0500

    Added git ignore and function to create isosceles triangle.

---
+++ src/com/mm/Drawer.java
@@ -0,0 +1,49 @@
+package com.mm;
+
+public class Drawer {
+
+    public void printOne() {
+        System.out.println("*");
+    }
+
+    public void drawHorizontalStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public void drawHorizontalSpaces(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    public void drawVerticalLine(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.println("*");
+        }
+    }
+
+    public void drawRightTriangle(int number) {
+        for (int i = 1; i <= number + 1; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private void drawIsosTriangle(int number, int constant) {
+        if (number != 1){
+            drawIsosTriangle(number-1, constant);
+        }
+        drawHorizontalSpaces(Math.abs(number-constant));
+        drawHorizontalStars(2*number-1);
+        System.out.println();
+    }
+
+    public void drawIsosTriangle(int number) {
+        drawIsosTriangle(number, number);
+    }
+}
+
---
+++ src/com/mm/Main.java
@@ -0,0 +1,11 @@
+package com.mm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Drawer drawer = new Drawer();
+
+        //Draw an Isosceles Triangle
+        drawer.drawIsosTriangle(5);
+    }
+}
